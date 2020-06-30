14e0a079a7a974c81f5198d2f742609e8049f018
edumco <edu.pombos@hotmail.com>
2020-06-30 03:39:49 -0300

    Re-enable tests

--- basic-framework/src/test/java/mco/maven/framework/tests/HomeTests.java
+++ basic-framework/src/test/java/mco/maven/framework/tests/HomeTests.java
@@ -1,28 +1,35 @@
 package mco.maven.framework.tests;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 import mco.maven.framework.core.Browser;
 import mco.maven.framework.pages.Home;
 
 public class HomeTests {
 
+	@Test
 	public void checkBrowserTitle() {
 
 		Browser.navigateTo(Home.URL);
 		assertTrue("Title contains google", Browser.title().equalsIgnoreCase(Home.TITLE));
 	}
 
+	@Test
 	public void checkBrowserURL() {
 
 		Browser.navigateTo(Home.URL);
-		assertTrue("Redirect correctly ", Browser.url().equalsIgnoreCase(Home.FULL_URL));
+		assertTrue("Redirect correctly ", Browser.url().startsWith(Home.URL));
 	}
 
+	@Test
 	public void checkNotAuthenticated() {
 
+		Browser.url();
 		Browser.navigateTo(Home.URL);
-		assertTrue("User is not not authenticated", Home.isAuthenticated() == false);
+		assertFalse("User is not authenticated", Home.isAuthenticated());
 	}
 
 }
